1f3de03dd01f64a08148005da9e1aadd4ae519f9
BDunnCode <BDunnCode@github.com>
2023-11-25 23:18:17 -0600

    second commit

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('#board')
-  const score = document.querySelector('#score')
+let currentMoleTile
 
 
+window.onload = function() {
+  setGame();
+}
 
+function setGame() {
+    for (let i = 0; i < 9; i++) {
+      let tile = document.createElement("div")
+      tile.id = i.toString()
+      document.getElementById("board").appendChild(tile)
+    }
 
+}
 
+function getRandomTile() {
+  let num = Math.floor(Math.random * 9)
+  return num.toString()
+}
 
+function setMole() {
 
+  let mole = document.createElement("img")
+  mole.src = "./img/monty-mole.png"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+  let num = getRandomTile()
+}
