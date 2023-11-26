4d32fc3a6e9ecd1325830fee674b2547767acb27
BDunnCode <BDunnCode@github.com>
2023-11-26 16:06:28 -0600

    finish game

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let currMoleTile
-let currPlantTile
+let currPiranhaPlantTile
+let score = 0
+let gameOver = false
 
 window.onload = function() {
   setGame();
@@ -9,6 +11,7 @@ function setGame() {
     for (let i = 0; i < 9; i++) {
       let tile = document.createElement("div")
       tile.id = i.toString()
+      tile.addEventListener('click', selectTile)
       document.getElementById("board").appendChild(tile)
     }
 
@@ -23,28 +26,55 @@ function getRandomTile() {
 }
 
 function setMole() {
+  if (gameOver) {
+    return
+  }
   if (currMoleTile) {
     currMoleTile.innerHTML = ""
   }
 
 
-  mole = document.createElement("img")
+  let mole = document.createElement("img")
   mole.src = "./img/monty-mole.png"
 
   let num = getRandomTile()
+  if (currPiranhaPlantTile && currPiranhaPlantTile.id == num) {
+    return
+  }
   currMoleTile = document.getElementById(num)
   currMoleTile.appendChild(mole)
 }
 
 function setPiranhaPlant() {
-  if (currMoleTile) {
-    currMoleTile.innerHTML = ""
+  if (gameOver) {
+    return
   }
 
-  piranhaPlant = document.createElement("img")
+  if (currPiranhaPlantTile) {
+    currPiranhaPlantTile.innerHTML = ""
+  }
+
+  let piranhaPlant = document.createElement("img")
   piranhaPlant.src = "./img/piranha-plant.png"
 
   let num = getRandomTile()
-  currPiranhaPlant = document.getElementById(num)
-  currPiranhaPlantTile.appendChild(mole)
+  if (currMoleTile && currMoleTile.id == num) {
+    return
+  }
+  currPiranhaPlantTile = document.getElementById(num)
+  currPiranhaPlantTile.appendChild(piranhaPlant)
+}
+
+function selectTile() {
+  if (gameOver) {
+    return
+  }
+
+  if (this == currMoleTile) {
+    score += 10
+    document.getElementById("score").innerText = score.toString()
+  } else if (this == currPiranhaPlantTile) {
+    document.getElementById("score").innerText = "GAME OVER " + score
+    gameOver = true
+  }
 }
