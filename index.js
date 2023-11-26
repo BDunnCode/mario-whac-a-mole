e1effa04aef0c63b31d6e81a93d546918a1e7bc9
BDunnCode <BDunnCode@github.com>
2023-11-26 15:28:26 -0600

    fixed some git issues

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let currMoleTile
-let currPlantTile
+let currPiranhaPlantTile
+let score = 0
+let gameOver = false
 
 window.onload = function() {
   setGame();
@@ -9,14 +11,16 @@ function setGame() {
     for (let i = 0; i < 9; i++) {
       let tile = document.createElement("div")
       tile.id = i.toString()
+      tile.addEventListener("click", () => {
+        if (this == currMoleTile) {
+        score++
+        }
+      })
       document.getElementById("board").appendChild(tile)
     }
 
     setInterval(setMole, 1000)
     setInterval(setPiranhaPlant, 2000)
-=======
-window.onload = function() {
-  setGame();
 }
 
 function getRandomTile() {
@@ -32,23 +36,26 @@ function setMole() {
   let mole = document.createElement("img")
   mole.src = "./img/monty-mole.png"
 
-  mole = document.createElement("img")
-  mole.src = "./img/monty-mole.png"
-
   let num = getRandomTile()
+  if (currPiranhaPlantTile && currPiranhaPlantTile.id == num) {
+    return
+  }
   currMoleTile = document.getElementById(num)
   currMoleTile.appendChild(mole)
 }
 
 function setPiranhaPlant() {
-  if (currMoleTile) {
-    currMoleTile.innerHTML = ""
+  if (currPiranhaPlantTile) {
+    currPiranhaPlantTile.innerHTML = ""
   }
 
-  piranhaPlant = document.createElement("img")
+  let piranhaPlant = document.createElement("img")
   piranhaPlant.src = "./img/piranha-plant.png"
 
   let num = getRandomTile()
-  currPiranhaPlant = document.getElementById(num)
-  currPiranhaPlantTile.appendChild(mole)
+  if (currMoleTile && currMoleTile.id == num) {
+    return
+  }
+  currPiranhaPlantTile = document.getElementById(num)
+  currPiranhaPlantTile.appendChild(piranhaPlant)
 }
